084bf41e3b0637ed3075426ce6029d43fea86120
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2025-02-12 23:47:23 -0600

    Track is ready.

---
+++ src/tracks/create-track.dto.ts
@@ -0,0 +1,6 @@
+export class CreateTrackDto {
+  name: string;
+  album: string;
+  duration: string;
+  track_number: number;
+}
--- src/tracks/tracks.controller.ts
+++ src/tracks/tracks.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Query } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Track, TrackDocument } from '../schemas/track.schema';
+import { Model } from 'mongoose';
+import { Album, AlbumDocument } from '../schemas/album.schema';
+import { CreateTrackDto } from './create-track.dto';
 
 @Controller('tracks')
-export class TracksController {}
+export class TracksController {
+  constructor(
+    @InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+    @InjectModel(Album.name) private albumModel: Model<AlbumDocument>,
+  ) {}
+
+  @Get()
+  async getAllTracks(@Query('album') trackId: string) {
+    if (trackId) {
+      const trackAlbum = await this.trackModel.find({ album: trackId });
+      return trackAlbum;
+    }
+    return this.trackModel.find();
+  }
+
+  @Post()
+  async create(@Body() trackDto: CreateTrackDto) {
+    const album = await this.albumModel.findById(trackDto.album);
+    if (!album) throw new NotFoundException('Album not found');
+    const newTrack = new this.trackModel({
+      name: trackDto.name,
+      album: trackDto.album,
+      duration: trackDto.duration,
+      track_number: trackDto.track_number,
+    });
+    return await newTrack.save();
+  }
+
+  @Delete(':id')
+  async deleteTrack(@Param('id') id: string) {
+    const deletedTrack = await this.trackModel.findByIdAndDelete(id);
+    return { message: 'Track deleted', deletedTrack };
+  }
+}
