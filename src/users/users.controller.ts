b939ef8dceaefdb1a80b9d8537f9f1e30513255c
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2025-02-15 14:18:47 -0600

    HW 94 - Token authorization added.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from '../schemas/user.schema';
 import { Model } from 'mongoose';
 import { RegisterUserDto } from './register-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { TokenAuthGuard } from '../token-auth/token-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -17,5 +20,17 @@ export class UsersController {
 
     user.generateToken();
     return await user.save();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('sessions')
+  login(@Req() req: Request<{ user: User }>) {
+    return req.user;
+  }
+
+  @UseGuards(TokenAuthGuard)
+  @Get('secret')
+  secret(@Req() req: Request<{ user: User }>) {
+    return { user: req.user, message: 'This is a secret message' };
   }
 }
